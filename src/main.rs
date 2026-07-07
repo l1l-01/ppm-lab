e5a008553902a751dcdd7162b3a7e0c261cb3d2c
l1l <khalil23ar@gmail.com>
2026-07-07 19:55:17 +0100

    Fix parser

--- src/main.rs
+++ src/main.rs
@@ -6,21 +6,40 @@ fn parser(args: Vec<String>){
     let img: String = fs::read_to_string(&args[1]).expect("Failed to read file!");
     let sensitized_data: String  = img.replace("\n", " ");
 
-    let mut i: u64 = 1;
-    let mut metadata: Vec<&str> = vec![];
-    let mut pixels: Vec<u8> = vec![];
 
+    let mut i: u64 = 1;
+    let mut metadata: Vec<u16> = vec![];
+    let mut pixels: Vec<Vec<u8>> = vec![];
+    
+
+    let mut pix_increment: u8 = 0;
+    let mut one_pixel: Vec<u8> = vec![];
     for word in sensitized_data.split(" ")  {
         if !word.is_empty() {
-            match i {
-                1 | 2 | 3 | 4 => metadata.push(word),
-                _ => pixels.push(word.parse().unwrap())
+            let val: u8 = match word.parse() {
+                Ok(val) => val,
+                Err(_) => continue
+            };
+            
+            if pix_increment == 3 {
+                pix_increment = 0;
+                pixels.push(one_pixel.clone());
+                one_pixel.clear();
+
+                if i == 3 {
+                    let data: u16 = val as u16;
+                    metadata.push(data);
+                }
+            }else {
+                one_pixel.push(val.clone());
+                i += 1;
             }
-            i += 1;
+
+            pix_increment += 1;
         }
     }
     
-    let num_pixels: u64 = pixels.len() as u64 / 3;
+    let num_pixels: u64 = pixels.len() as u64;
     println!("Number of pixels: {:?}", num_pixels);
 }
 
