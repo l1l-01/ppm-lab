307d582211670f4afd6bb88a7f3410a7ae69578e
l1l <khalil23ar@gmail.com>
2026-07-08 17:15:41 +0100

    Improve pixel collection in the parser

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,53 @@
 use std::{env, fs};
 
+pub struct Pixcel {
+    r: u8,
+    g: u8,
+    b: u8,
+}
+
 fn parser(args: Vec<String>){
     fs::exists(&args[1]).expect("File path is wrong or file doesn't exist!");
+    fs::exists(&args[2]).expect("Filter wasn't provided!");
 
     let img: String = fs::read_to_string(&args[1]).expect("Failed to read file!");
     let sensitized_data: String  = img.replace("\n", " ");
 
 
-    let mut i: u64 = 1;
     let mut metadata: Vec<u16> = vec![];
-    let mut pixels: Vec<Vec<u8>> = vec![];
+    let mut pixels: Vec<Pixcel> = vec![];
     
-
-    let mut pix_increment: u8 = 0;
     let mut one_pixel: Vec<u8> = vec![];
-    for word in sensitized_data.split(" ")  {
-        if !word.is_empty() {
-            let val: u8 = match word.parse() {
-                Ok(val) => val,
-                Err(_) => continue
-            };
-            
-            if pix_increment == 3 {
-                pix_increment = 0;
-                pixels.push(one_pixel.clone());
-                one_pixel.clear();
+    for word in sensitized_data.split_whitespace() {
+        let val: u16 = match word.parse() {
+            Ok(v) => v,
+            Err(_) => continue,
+        };
 
-                if i == 3 {
-                    let data: u16 = val as u16;
-                    metadata.push(data);
-                }
-            }else {
-                one_pixel.push(val.clone());
-                i += 1;
-            }
+        // Parse the header
+        if metadata.len() < 3 {
+            metadata.push(val);
+            continue;
+        }
 
-            pix_increment += 1;
+        // Collected enough values to construct a pixel
+        one_pixel.push(val as u8);
+
+        // Add one pixel at a time to pixels
+        if one_pixel.len() == 3 {
+            pixels.push(Pixcel {
+                r: one_pixel[0],
+                g: one_pixel[1],
+                b: one_pixel[2],
+            });
+
+            one_pixel.clear();
         }
     }
     
     let num_pixels: u64 = pixels.len() as u64;
     println!("Number of pixels: {:?}", num_pixels);
+    println!("Meta Data: {:?}", metadata);
 }
 
 fn main() {
