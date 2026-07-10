40ef20067b79618a25795e092f566f617024f9bf
l1l <khalil23ar@gmail.com>
2026-07-10 19:03:46 +0100

    Refactor code

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ pub struct Pixcel {
     b: u8,
 }
 
-fn parser(args: Vec<String>) -> (Vec<Pixcel>, String) {
+fn parser(args: Vec<String>) -> (Vec<Pixcel>, Vec<u16>) {
     let img: String = fs::read_to_string(&args[1]).expect("Failed to read file!");
     let sensitized_data: String  = img.replace("\n", " ");
 
@@ -45,24 +45,22 @@ fn parser(args: Vec<String>) -> (Vec<Pixcel>, String) {
         }
     }
     
-    let num_pixels: u64 = pixels.len() as u64;
-    println!("Image width: {:?}\nHeight {:?}\nMax Value: {:?}\nNumber of pixels: {:?}", metadata[0], metadata[1], metadata[2],num_pixels);
+    println!("Image width: {:?}\nHeight {:?}\nMax Value: {:?}\nNumber of pixels: {:?}", metadata[0], metadata[1], metadata[2],pixels.len());
 
-    let data:String = format!("P3\n{} {} {}\n", metadata[0],metadata[1],metadata[2]);
-
-    return (pixels, data);
+    return (pixels, metadata);
 }
 
 fn grayscale(pixels: &[Pixcel],mut data: String) {
     for pixel in pixels {
         let eff: u8 = ((0.2627 * pixel.r as f32) +  (0.6780 * pixel.g as f32) + (0.0593 * pixel.b as f32)) as u8;
-        let _ = writeln!(data, "{} {} {}", eff, eff, eff);
+        let _ = writeln!(data, "{} {} {}", &eff, &eff, &eff);
     }
 
-    create_img(data);
+    let mut file: File = File::create("grayscale.ppm").expect("Failed to create file");
+    file.write_all(data.as_bytes()).expect("Failed to write image!");
 }
 
-fn invert(pixels: &[Pixcel], mut data: String){
+fn invert(pixels: &[Pixcel], mut data: String) {
     for pixel in pixels {
         let r: u8 = 255 - pixel.r;
         let g: u8 = 255 - pixel.g;
@@ -70,11 +68,7 @@ fn invert(pixels: &[Pixcel], mut data: String){
         let _ = writeln!(data, "{} {} {}", r, g, b);
     }
 
-    create_img(data);
-}
-
-fn create_img(data: String) {
-    let mut file: File = File::create("img.ppm").expect("Failed to create file");
+    let mut file: File = File::create("invert.ppm").expect("Failed to create file");
     file.write_all(data.as_bytes()).expect("Failed to write image!");
 }
 
@@ -87,9 +81,12 @@ fn main() {
     
     let filter = args[2].clone();
     let (pixels, metadata) = parser(args);
+
+    let data:String = format!("P3\n{} {} {}\n", metadata[0],metadata[1],metadata[2]);
+
     match filter.as_str() {
-        "grayscale" => grayscale(&pixels,metadata),
-        "invert" => invert(&pixels,metadata),
+        "grayscale" => grayscale(&pixels, data),
+        "invert" => invert(&pixels, data),
         _ => panic!("Please use a supported filter!"),
     }
 
